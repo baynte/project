481ba67348996abe8721ccdf0b95af0833acdc4a
Jolan <jolan.owang@gmail.com>
2020-07-27 13:57:47 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/user', 'api\LoginController@login');
+Route::post('user', 'api\LoginController@login');
 
 Route::apiResource('crud', 'api\CrudController')->middleware('auth:api');
