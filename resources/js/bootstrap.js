fd8d16839fae74dbb3471b4ef135a15c6f51d1cb
Jolan <jolan.owang@gmail.com>
2020-07-27 12:12:51 +0800

    update app.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,7 +24,7 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 
-
+window.navigator = navigator;
 window.faceapi = require('face-api.js');
 
 /**
